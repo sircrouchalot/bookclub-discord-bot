92b38507f7ece30d66186d91d6962fd90c57d21d
Alex Crouch <acrouch06@gmail.com>
2023-04-07 15:53:49 -0500

    create /vote command

---
+++ src/commands/functions/vote.js
@@ -0,0 +1,41 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { StringSelectMenuBuilder, ActionRowBuilder } = require('discord.js');
+const Books = require("../../data/models/Books.js");
+const { QueryTypes } = require('sequelize');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('vote')
+        .setDescription('Use this command to vote for the Book of the Month')
+        .setDMPermission(false),
+    async execute(interaction) {
+        // Get all dates in Books that do not have entries in Botms
+        const dates = await Books.sequelize.query("SELECT DISTINCT books.date, books.month_string FROM bookclub_database.books books WHERE books.date not in (SELECT botms.date FROM bookclub_database.botms botms) ORDER BY date DESC", {type: QueryTypes.SELECT});
+
+        let stringSelect = [];
+
+        if ((dates) && dates.length > 0) {
+            for (const entry in dates) {
+                stringSelect.push({
+                    label: `${dates[entry].month_string}`,
+                    value: `${dates[entry].month_string}`
+                })
+
+            };
+        }
+            
+        // Choose Month from list of months in database
+        const select = new ActionRowBuilder().addComponents(
+            new StringSelectMenuBuilder()
+                .setCustomId('voteMonthSelect')
+                .setPlaceholder('Select Month')
+                .addOptions(stringSelect),
+        );
+        await interaction.reply({
+            content: "",
+            components: [select],
+            ephemeral: true
+        });
+        
+    }
+}
