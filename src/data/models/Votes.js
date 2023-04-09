ec1dca3d366b5499777088b2971de492b6d0d945
Alex Crouch <acrouch06@gmail.com>
2023-04-09 06:30:34 -0500

    created Votes table model

---
+++ src/data/models/Votes.js
@@ -0,0 +1,31 @@
+const { Sequelize, DataTypes } = require('sequelize');
+
+const sequelize = require(`../config/database.js`);
+
+const Votes = sequelize.define('votes', {
+    vote_uid: {
+        type: DataTypes.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+        primaryKey: true
+    },
+    guild_id: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    month_string: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    user: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    votes: {
+        type: DataTypes.TEXT('medium'),
+        allowNull: true,
+    }
+
+});
+
+module.exports = Votes;
