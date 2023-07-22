b03853bd2b9b3f24c0135f5edfcc8deaf5b33cab
Alex Crouch <acrouch06@gmail.com>
2023-07-22 12:01:16 -0500

    defined /getBotm command

--- src/commands/info/getBotm.js
+++ src/commands/info/getBotm.js
@@ -1,24 +1,55 @@
 const { SlashCommandBuilder } = require('discord.js');
+const { StringSelectMenuBuilder, ActionRowBuilder } = require('discord.js');
+const Botms = require("../../data/models/Botms.js");
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('getbotm')
 		.setDescription('Provides information about the current book of the month.')
-		.setDMPermission(false),
+		.setDMPermission(false)
 		// .addStringOption(option =>
 		// 	option.setName('month')
 		// 		.setDescription('Type in which month you need the book for (mm/yyyy).')
 		// ),
-	
+		,
 	async execute(interaction) {
-		
-		// Choose month from list of months in database
+		// Grab Months from db
+		const monthList = await Botms.findAll({
+			attributes: ['month_string', 'date'],
+			where: {
+				guild_id: interaction.guild.id,
+				
+			},
+			order: [['date', 'DESC']]
+		})
 
-		// Get Book from Botm table for that month and reply in a message
-		
-		// interaction.guild is the object representing the Guild in which the command was run
+		let monthSelect = [];
+
+		if ((monthList) && Object.keys(monthList).length > 0) {
+			for (const entry in monthList) {
+				monthSelect.push({
+					label: `${monthList[entry].month_string}`,
+					value: `${monthList[entry].month_string}//${monthList[entry].date}`
+				})
+			}
+		} else {
+			return await interaction.reply({
+				content: "There are no Books chosen. Please vote and select a Book of the Month and try again.",
+				ephemeral: true
+			})
+		}
+
+		// Choose month from list of months in database
+		const monthDrop = new ActionRowBuilder().addComponents(
+			new StringSelectMenuBuilder()
+			.setCustomId('botmMonthSelect')
+			.setPlaceholder('Select Month')
+			.addOptions(monthSelect),
+		);
+
 		await interaction.reply({
-			content: `${month}'s book has not been set.`,
+			content: "",
+			components: [monthDrop],
 			ephemeral: true
 		});
 	},
