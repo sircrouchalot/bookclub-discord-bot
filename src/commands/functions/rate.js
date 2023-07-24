b1262b5153156df78305c6aa229fa3346672cc8c
Alex Crouch <acrouch06@gmail.com>
2023-07-23 23:51:12 -0500

    added /rate command

--- src/commands/functions/rate.js
+++ src/commands/functions/rate.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder } = require('discord.js');
-const { StringSelectMenuBuilder, TextInputBuilder, TextInputStyle } = require('discord.js');
-
+const { StringSelectMenuBuilder, ActionRowBuilder } = require('discord.js');
+const Botms = require("../../data/models/Botms");
+const { Sequelize } = require('sequelize');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -9,18 +10,47 @@ module.exports = {
         .setDMPermission(false),
     async execute(interaction) {
 
-        //TODO: Confirm Month
+        // Get all dates for votes in Votes table
+        const dates = await Botms.findAll({
+            attributes: [
+                [Sequelize.fn('DISTINCT', Sequelize.col('date')), 'date'],
+                'month_string'
+            ],
+            where: {
+                guild_id: interaction.guild.id,
+            },
+            order: [['date', 'DESC']]
+        });
 
-        //TODO: Grab all books for selected month
+        let stringSelect = [];
 
-        const bookInput = new StringSelectMenuBuilder()
-            .setCustomId('botmInput')
-            .setRequired(true)
-            .setLabel(`Select which book`)
-            .addOptions (
-                {
-                    //TODO: Add options
-                }
-            )
+        if ((dates) && Object.keys(dates).length > 0) {
+            for (const entry in dates) {
+                stringSelect.push({
+                    label: `${dates[entry].month_string}`,
+                    value: `${dates[entry].month_string}//${dates[entry].date}`
+                })
+
+            };
+        } else {
+            // return await interaction.reply({
+            //     content: "There was an error. Not sure what happened. If it keeps happening, let Alex know.",
+            //     ephemeral: true
+            // })
+        }
+
+        // Choose Month from list of months in database
+        const monthSelect = new ActionRowBuilder().addComponents(
+            new StringSelectMenuBuilder()
+                .setCustomId('rateMonthSelect')
+                .setPlaceholder('Select Month')
+                .addOptions(stringSelect),
+        );
+
+        await interaction.reply({
+            content: "",
+            components: [monthSelect],
+            ephemeral: true
+        });
     }
 }
